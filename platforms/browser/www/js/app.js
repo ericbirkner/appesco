fd50d75c94b41ee143b0da52cb5f8eb4ed78363a
Eric Birkner <ericbirkner@gmail.com>
2018-10-20 21:41:23 -0300

    Initial commit

---
+++ platforms/browser/www/js/app.js
@@ -0,0 +1,34 @@
+var animateApp = angular.module('animateApp', ['ngRoute', 'ngAnimate']);
+
+animateApp.config(function($routeProvider) {
+    $routeProvider
+    	.when('/', {
+    		templateUrl: 'page-home.html',
+            controller: 'mainController'
+    	})
+      .when('/registro', {
+    		templateUrl: 'page-registro.html',
+            controller: 'aboutController'
+    	})
+      .when('/about', {
+    		templateUrl: 'page-about.html',
+            controller: 'aboutController'
+    	})
+    	.when('/contact', {
+    		templateUrl: 'page-contact.html',
+            controller: 'contactController'
+    	});
+
+});
+
+animateApp.controller('mainController', function($scope) {
+    $scope.pageClass = 'page-home';
+});
+
+animateApp.controller('aboutController', function($scope) {
+    $scope.pageClass = 'page-about';
+});
+
+animateApp.controller('contactController', function($scope) {
+    $scope.pageClass = 'page-contact';
+});
