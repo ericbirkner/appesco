251e8989c12e35689c48365f08c955be79a8ed7b
Eric Birkner <ericbirkner@gmail.com>
2018-11-13 23:06:26 -0300

    android version

---
+++ platforms/android/assets/www/js/index.js
@@ -0,0 +1,38 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// PhoneGap is ready
+//
+function onDeviceReady() {
+	// Empty
+}
+
+// alert dialog dismissed
+function alertDismissed() {
+	// do something
+}
+
+// Show a custom alert
+//
+function showAlert(msg) {
+	//cambiar cuando se testea por navegador
+	
+	navigator.notification.alert(
+		msg,  // message
+		alertDismissed,         // callback
+		'Pesco App',            // title
+		'Cerrar'                  // buttonName
+	);
+	
+	//alert(msg);
+	
+	
+}
+
+$('#main-menu li').each(function(){
+   //$('#main-menu-mobile').append($(this).clone(true)); 
+});
+$('nav .button-collapse').sideNav({
+    closeOnClick: true
+});
+
+
